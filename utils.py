c39ee33d124b222836218c47c61a81aeb4efe0be
qedpi <junruihu@gmail.com>
2018-04-17 18:59:31 -0700

    conditional extraction

--- utils.py
+++ utils.py
@@ -1,16 +1,18 @@
+import os
 import tarfile
 # NLP
 import gensim
 
-WORD_LIMIT = 100000
-WORD2VEC_TAR = 'GoogleNews-vectors-negative300-top100000.tar.gz'
 WORD2VEC_WEIGHTS = 'GoogleNews-vectors-negative300-top100000.bin'
+if WORD2VEC_WEIGHTS not in os.listdir():
+    WORD_LIMIT = 100000
+    WORD2VEC_TAR = 'GoogleNews-vectors-negative300-top100000.tar.gz'
 
-# extract word2vec zipped file (circumvent github max file size)
-tar = tarfile.open(WORD2VEC_TAR, "r:gz")
-tar.extractall()
-tar.close()
-print('finished extracting')
+    # extract word2vec zipped file (circumvent github max file size)
+    tar = tarfile.open(WORD2VEC_TAR, "r:gz")
+    tar.extractall()
+    tar.close()
+    print('finished extracting')
 
 # init word2vec model, limit to common words
 model = gensim.models.KeyedVectors.load_word2vec_format(WORD2VEC_WEIGHTS, binary=True, limit=WORD_LIMIT)
